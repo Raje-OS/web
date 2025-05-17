89f08feace3cf1cb23d5cc3e893c335c4ca27caf
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 14:09:37 -0500

    Update platform.service.service.ts

--- src/app/contents/platforms/services/platform.service.service.ts
+++ src/app/contents/platforms/services/platform.service.service.ts
@@ -2,19 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {environment} from '../../../../environments/environment';
+import { Platform } from '../model/platform.entity';
+import { map } from 'rxjs/operators';
 
+interface DbResponse {
+  platforms: Platform[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlatformService {
-  private baseUrl = environment.serverBaseUrl + environment.platformEndpointPath;
+  private baseUrl = environment.platformEndpointPath;
 
   constructor(private http: HttpClient) {}
 
-  getPlatformsByIds(ids: string[]): Observable<any[]> {
-    const query = ids.map(id => `id=${id}`).join('&');
-    return this.http.get<any[]>(`${this.baseUrl}?${query}`);
+  getPlatformsByIds(ids: string[]): Observable<Platform[]> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const platforms = data.platforms || [];
+        return platforms.filter(platform => ids.includes(platform.id));
+      })
+    );
   }
 
+  getAllPlatforms(): Observable<Platform[]> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => data.platforms || [])
+    );
+  }
+
+  getPlatformById(id: string): Observable<Platform | {}> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const platform = data.platforms.find(platform => platform.id === id);
+        return platform ? platform : {};
+      })
+    );
+  }
 }
